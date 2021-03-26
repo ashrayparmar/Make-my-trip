da71dfdaac276a57d8095e4f51d597df97d5fbfc
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-03-26 14:00:21 +0530

    feat: header design done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 import logo from "./logo.png"
+import logo1 from "./logo1.png"
 
 function App() {
 
+  function fetchData(){}
+
   return (
-    <div className="App">
       <div className="header">
-        <img className="img-logo" src={logo} alt=""></img>
+        <img className="img-logo" src={logo1} alt=""></img>
 
-        <div className="content">
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-        </div>
+        <div className="background">
+          
+          <form onSubmit={fetchData}>
+              
+              <div className="content">
 
+                <input placeholder="From"></input>
+                <div className="divider"></div>
+                <input placeholder="TO"></input>
+                <div className="divider"></div>
+                <input className="date" type="date"></input>
+                <div className="divider"></div>
+                <input className="travel-input" placeholder="Travellers" type="number"></input>
+
+                <div className="btn-search">
+                
+                <button>Search</button>
+
+                </div> 
+              </div>       
+          </form>
+        
       </div>
+
     </div>
   );
 }
