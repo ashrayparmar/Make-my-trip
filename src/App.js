30a0f6d56fe887e90006eb160fc7dd0697c2a0f8
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-03-26 12:24:14 +0530

    feat: header design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import logo from "./logo.png"
 
 function App() {
+
   return (
     <div className="App">
-      
+      <div className="header">
+        <img className="img-logo" src={logo} alt=""></img>
+
+        <div className="content">
+          <input></input>
+          <input></input>
+          <input></input>
+          <input></input>
+        </div>
+
+      </div>
     </div>
   );
 }
