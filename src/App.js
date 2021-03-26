ebfd7464d1dbf9f6e8b873eade343010127de527
Ashray Parmar <ashrayparmar0307@gmail.com>
2021-03-26 20:17:37 +0530

    DONE :)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {useState} from 'react';
 import './App.css';
 import logo1 from "./logo1.png"
+import flight from "./flight.png"
+import hotel from "./hotel.png"
+import villa from "./villa.png"
+import holiday from "./holiday.png"
+import train from "./train.png"
+import bus from "./bus.png"
+import visa from "./visa.png"
+import cabs from "./cabs.png"
+import plane2 from "./plane2.png"
 
 function App() {
 
   const [showFlights, setShowFlights] = useState([])
 
-  const delhiToBanglore = {
+  const delhiToBangalore = {
     arrival: "Delhi",
-    destination: "Banglore",
+    destination: "Bangalore",
     arrivalTime: "5:15",
     destinationTime: "7:55",
     flight: "INDIGO",
@@ -18,9 +27,9 @@ function App() {
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/IndiGo_logo.svg/250px-IndiGo_logo.svg.png"
   };
 
-  const delhiToBanglore1 = {
+  const delhiToBangalore1 = {
     arrival: "Delhi",
-    destination: "Banglore",
+    destination: "Bangalore",
     arrivalTime: "2:15",
     destinationTime: "4:05",
     flight: "Air Asia",
@@ -30,9 +39,45 @@ function App() {
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/AirAsia_NewLogo.svg/1200px-AirAsia_NewLogo.svg.png"
   };
 
-  const delhiToBanglore2 = {
+  const delhiToBangalore2 = {
     arrival: "Delhi",
-    destination: "Banglore",
+    destination: "Bangalore",
+    arrivalTime: "5:10",
+    destinationTime: "5:50",
+    flight: "Spice Jet",
+    price: "3500 INR",
+    travelTime: "Non-stop",
+    duration: "1 hour 40 mins",
+    image: "https://airhex.com/images/airline-logos/tail/spicejet.png"
+  };
+
+  const delhiToMumbai = {
+    arrival: "Delhi",
+    destination: "Mumbai",
+    arrivalTime: "2:15",
+    destinationTime: "4:05",
+    flight: "Air Asia",
+    price: "3650 INR",
+    travelTime: "Non-stop",
+    duration: "1 hour 50 mins",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/AirAsia_NewLogo.svg/1200px-AirAsia_NewLogo.svg.png"    
+  };
+
+  const delhiToMumbai1 = {
+    arrival: "Delhi",
+    destination: "Mumbai",
+    arrivalTime: "5:15",
+    destinationTime: "7:55",
+    flight: "INDIGO",
+    price: "3400 INR",
+    travelTime: "Non-stop",
+    duration: "1 hour 30 mins",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/IndiGo_logo.svg/250px-IndiGo_logo.svg.png"
+  };
+
+  const delhiToMumbai2 = {
+    arrival: "Delhi",
+    destination: "Mumbai",
     arrivalTime: "5:10",
     destinationTime: "5:50",
     flight: "Spice Jet",
@@ -50,8 +95,13 @@ function App() {
     let dateOfJourney = e.target[2].value;
     let noOfTraveller = e.target[3].value; 
 
-    if( from.toLowerCase() === "delhi" && to.toLowerCase() === "banglore") {
-      setShowFlights([delhiToBanglore, delhiToBanglore1, delhiToBanglore2])
+    if( from.toLowerCase() === "delhi" && to.toLowerCase() === "bangalore") {
+      
+      setShowFlights([delhiToBangalore, delhiToBangalore1, delhiToBangalore2])
+    }
+
+    else if( from.toLowerCase() === "delhi" && to.toLowerCase() === "mumbai") {
+      setShowFlights([delhiToMumbai, delhiToMumbai1, delhiToMumbai2])
     }
 
   }
@@ -81,6 +131,20 @@ function App() {
                 </div> 
               </div>       
           </form>
+
+      </div>
+
+      <div className="top-content">
+
+      <img src={flight}></img>
+      <img src={hotel}></img>
+      <img src={villa}></img>
+      <img src={holiday}></img>
+      <img src={train}></img>
+      <img src={bus}></img>
+      <img src={visa}></img>
+      <img src={cabs}></img>
+      <img src={plane2}></img>
 
       </div>
 
